b1c887bd2a0c540e3d8891de54a51de1b2e1b8f1
Spencer Bykowsky <spbykowsky@gmail.com>
2018-10-29 16:11:07 -0400

    initial go at what my main app class should be

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,3 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+
+import Features from './components/Features.jsx';
+import TechSpecs from './components/TechSpecs.jsx';
+
+
+class Productdescriptions extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      view: "main",
+      descriptions: []
+    }
+  }
+
+  conponentDidMount(){
+    console.log("Mounted");
+    $.get('/productdescriptions',(data) =>{
+      console.log(data);
+
+      this.setState({
+        descriptions: data
+      })
+    })
+  }
+
+  changeView(tabs){
+    this.setState({
+      view:tabs
+    })
+  }
+
+  renderView() {
+    const {view} = this,state;
+    if(view === "features"){
+      return <Features />
+    }else if (view === "techSpecs"){
+      return <TechSpecs />
+    }
+  }
+
+  render() {
+    return(
+      <div>
+      {this.renderView()}
+      </div>
+      )
+  }
+}
+
+
+
+ReactDOM.render(<Productdescriptions />, document.getElementById('productDescriptions'));
+
